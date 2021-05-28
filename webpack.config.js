a131fbf881dbe0455ca6b012ff0bef166700b5e5
Ravindra <ravindra@Lap-FVFXHC6LHV22.local>
2021-05-28 13:13:06 +0530

    MAJOR: working react app

---
+++ webpack.config.js
@@ -0,0 +1,140 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+const path = require('path');
+
+const config = {
+  entry: ['react-hot-loader/patch', './src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            },
+          },
+          'postcss-loader',
+        ],
+      },
+      // NOTE: for antd theme tweaking
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              lessOptions: {
+                modifyVars: {
+                  hack: `true; @import "${path.resolve(
+                    __dirname,
+                    'src',
+                    'shared',
+                    'config',
+                    'theme.less'
+                  )}";`,
+                },
+                javascriptEnabled: true,
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: 'file-loader',
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      'react-dom': '@hot-loader/react-dom',
+      '@observsta': path.resolve(__dirname, 'src'),
+      '@observsta-elements': path.resolve(__dirname, 'src/shared/components'),
+      '@observsta-modules': path.resolve(__dirname, 'src/modules'),
+      '@observsta-shared': path.resolve(__dirname, 'src/shared'),
+    },
+  },
+  devServer: {
+    contentBase: './dist',
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Observsta',
+      template: path.resolve(__dirname, 'src/index.html'),
+      filename: 'index.html',
+    }),
+    // new Dotenv(),
+    new MiniCssExtractPlugin(),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+    }),
+    new webpack.ContextReplacementPlugin(
+      /date-fns[/\\]/,
+      new RegExp(
+        `[/\\\\](${['en-GB', 'hi', 'de', 'it', 'es', 'fr'].join(
+          '|'
+        )})[/\\\\]index.js$`
+      )
+    ),
+  ],
+  optimization: {
+    runtimeChunk: 'single',
+    // NOTE: splits up the bundle into two:
+    // one bundle contains the code we  write,
+    // and the other contains all your dependencies
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+};
+
+module.exports = (env, argv) => {
+  if (argv.hot) {
+    // Cannot use "contenthash" when hot reloading is enabled.
+    config.output.filename = '[name].[fullhash].js';
+  }
+
+  return config;
+};
